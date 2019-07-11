50fb655032069df976e477f58ebb4e23979c08a6
Dany M Pradana <danympradana@gmail.com>
2019-07-11 16:52:00 +0700

    Commit jwt auth

---
+++ backend/app/Http/Controllers/APIController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\User;
+use Hash;
+use JWTAuth;
+class APIController extends Controller
+{
+	
+
+    public function register(Request $request)
+    {        
+    	$input = $request->all();
+    	$input['password'] = Hash::make($input['password']);
+    	User::create($input);
+        return response()->json(['result'=>true]);
+    }
+    
+
+    public function login(Request $request)
+    {
+    	$input = $request->all();
+    	if (!$token = JWTAuth::attempt($input)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+    
+
+    public function get_user_details(Request $request)
+    {
+    	$input = $request->all();
+    	$user = JWTAuth::toUser($request->token);
+        return response()->json(['result' => $user]);
+    }
+
+    public function logout(Request $request)
+    {
+        JWTAuth::invalidate($request->token);
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh(Request $request){
+        $user = JWTAuth::toUser($request->token);
+        $token = JWTAuth::refresh($request->token);
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => $user->name
+        ]);
+    }
+    
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => auth()->user()->name
+        ]);
+    }
+}
--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -20,3 +20,16 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 Route::get('/balik-kalimat', 'HomeController@balikKalimat')->name('balik-kalimat');
 Route::post('/balik-kalimat-proses', 'HomeController@balikKalimatProses')->name('balik-kalimat-proses');
+
+Route::group([
+    'middleware' => ['api','cors'],
+    'prefix' => 'api'
+], function ($router) {
+    Route::post('register', 'APIController@register');
+    Route::post('login', 'APIController@login');
+    Route::group(['middleware' => 'jwt-auth'], function () {
+        Route::post('get_user_details', 'APIController@get_user_details');
+        Route::get('refresh', 'APIController@refresh');
+        Route::get('logout', 'APIController@logout');
+    });
+});
